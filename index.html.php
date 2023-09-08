836840ab17ff584363160da4df27d8ac89ae9d08
hichem mansouri <Hichem.m83@gmail.com>
2023-09-08 10:41:50 +0200

    version 1.03

--- index.html.php
+++ index.html.php
@@ -7,38 +7,41 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Formulaire de Contact</h1>
+
+        <div class="container">
+            <!-- <img src="img/blue_bkgrd.jpg" alt="back"> -->
+            <h1>Formulaire de Contact</h1>
+            <form action="Traitement.php" method="post">
+                <div class="form-groupe">
+                    <label for="nom">Nom et prénom :</label>
+                    <input type="text" id="nom" name="nom" placeholder = "Vos nom et prénom"required>
+                </div>
+                <div class="form-group">
+                    <label for="email"> Adresse mail :</label>
+                    <input type="email" id="email" name="email" placeholder = "Votre adresse mail" required>
+                </div>
+                <div class="form-group">
+                    <label for="tel">Téléphone :</label>
+                    <input type="tel" id="tel" name="tel" placeholder = "Indiquez votre numéro" required>
+                </div>
+                <div class="form-group">
+                    <label for="sujet">Sujet (*):</label>
+                    <input type="text" id="sujet" name="sujet" placeholder = "Le sujet de votre message" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message (*):</label>
+                    <textarea id="message" name="message" placeholder = "Veuillez entrer votre message ici" required></textarea>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <input type="checkbox" id="accepter" name="accepter" required> En soumettant ce formulaire, <span class = "bold">j'accepte la politique de confidentialité</span> du site.
+                    </label>
+                </div>
+                <p class="caption">(*) Champs obligatoires</p>
+                <button type="submit">Valider</button>
+            </form>
         
-        <form action="Traitement.php" method="post">
-            <div class="form-groupe">
-                <label for="nom">Nom et prénom :</label>
-                <input type="text" id="nom" name="nom" placeholder = "Vos nom et prénom"required>
-            </div>
-            <div class="form-group">
-                <label for="email"> Adresse mail :</label>
-                <input type="email" id="email" name="email" placeholder = "Votre adresse mail" required>
-            </div>
-            <div class="form-group">
-                <label for="tel">Téléphone :</label>
-                <input type="tel" id="tel" name="tel" placeholder = "Indiquez votre numéro" required>
-            </div>
-            <div class="form-group">
-                <label for="sujet">Sujet (*):</label>
-                <input type="text" id="sujet" name="sujet" placeholder = "Le sujet de votre message" required>
-            </div>
-            <div class="form-group">
-                <label for="message">Message (*):</label>
-                <textarea id="message" name="message" placeholder = "Veuillez entrer votre message ici" required></textarea>
-            </div>
-            <div class="form-group">
-                <label>
-                    <input type="checkbox" id="accepter" name="accepter" required> En soumettant ce formulaire, <span class = "bold">j'accepte la politique de confidentialité</span> du site.
-                </label>
-            </div>
-            <p class="caption">(*) Champs obligatoires</p>
-            <button type="submit">Valider</button>
-        </form>
-    </div>
+        </div>
+    </section>
 </body>
 </html>
--- traitement.php
+++ traitement.php
@@ -8,7 +8,7 @@
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
-
+    //dossier vendor ou mettre tout les fichier comme bootstrap, dossier php mailer, c'est une convention
 
 //Create an instance; passing `true` enables exceptions
 
@@ -19,12 +19,12 @@
     $sujet = $_POST['sujet'];
     $message = $_POST['message'];
 
-    $message = 'nom:'.$nom."\n"."Email : ".$email."\n".'tel:'.$tel."\n"."message :".$message; 
+    $message = 'nom: '.$nom."<br>"."Email: ".$email."<br>".'tel:'.$tel."<br>".'sujet: '.$sujet.'<br>'."message: ".$message; 
 try {
 
 //option de serveur
-    $mail = new PHPMailer($nom, $email,$tel, $sujet, $message);//on creer un objet phpmailer pour utiliser ses attribut et méthode         
-    $mail->isSMTP();                                  //envoi SMTP utilisé
+    $mail = new PHPMailer(true);//on créé un objet phpmailer pour utiliser ses attributs et méthodes         
+    $mail->isSMTP();                                  //envoi en SMTP 
     $mail->Host       = 'smtp.gmail.com';             //parametre le serveur SMTP pour envoyer par gmail
     $mail->SMTPAuth   = true;                         //SMTP authentification
     $mail->Username   = 'hichem.m83@gmail.com';       //Permet d'accder a l'attribut username de l'objet créé(c'est le mail qui sert à envoyer des mails)
@@ -34,19 +34,21 @@ try {
 
     //Recipients
     $mail->setFrom($email, $nom);      //le message est envoyé par $mail et son nom par $name
-    $mail->addAddress('hichem.m83@gmail.com');         //ajoute un recepteur(qui receptionne les mails des users), ajoute un nom (facultatif) 
+    $mail->addReplyTo($email, $nom);
+    $mail->addAddress('hichem.m83@gmail.com');//ajoute un recepteur(qui receptionne les mails des users), ajoute un nom (facultatif) 
 
+    
     //Content
-    $mail->isHTML(true);                 //paramettrer le format de l'email en HTML
-    $mail->Subject = 'sujet du message de la personne';
+    $mail->isHTML(true);     //parametre le format de l'email en HTML
+    $mail->Subject = $sujet;
     $mail->Body = $message; //'c'est le corp du message qui est initialisé plus haut
     $mail->AltBody ="intitulé" ;  //'This is the body in plain text for non-HTML mail clients';
-
+    
     // pour envoyer le message on va utiliser la methode send() -->
 
     $mail->send();     
     echo 'message envoyé' ; 
-}catch (Exception $e){
-      echo 'message non envoyé:'.$mail->ErrorInfo;
+    }catch (Exception $e){
+    echo 'message non envoyé:';
   }
-?>
+?>+
